b08f228c1a4e572b582035f6ec16d1267d0039df
Mike <dev@mail.com>
2026-02-23 18:01:10 -0500

    Initial Commit

---
+++ OsxPaster/OsxPasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  OsxPasterApp.swift
+//  OsxPaster
+//
+//  Created by Mike Ruiz on 23/02/26.
+//
+
+import SwiftUI
+
+@main
+struct OsxPasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
